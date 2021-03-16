bdbdb7748ad34b23e6c2dadef284fffbac4a6882
Perttu Ehn <perttu.ehn@exove.fi>
2021-03-16 13:07:54 +0200

    Add basic checks to succeed with the command, or retun error code

--- docker/scripts/ld.command.solr-config-update.sh
+++ docker/scripts/ld.command.solr-config-update.sh
@@ -4,6 +4,15 @@
 # This file contains solr-config-update -command for local-docker script ld.sh.
 
 function ld_command_solr-config-update_exec() {
+    CONT_ID=$(find_container ${CONTAINER_SOLR:-solr})
+    if [ "$?" -eq "1" ]; then
+      echo -e "${Red}ERROR: Trying to locate a container with empty name.${Color_Off}"
+      return 1
+    fi
+    if [ -z "$CONT_ID" ]; then
+      echo -e "${Red}ERROR: Solr container ('${CONTAINER_SOLR:-solr}') is not up.${Color_Off}"
+      return 2
+    fi
 
     COMM="docker-compose exec ${CONTAINER_SOLR:-solr} bash -c \"cp /solr-config/conf/* /var/solr/data/${SOLR_CORE}/conf\""
     [ "$LD_VERBOSE" -ge "2" ] && echo -e "${Cyan}Next: $COMM${Color_Off}"

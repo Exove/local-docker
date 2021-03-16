2791f1016f40f684075035953d890a1289369ce6
Perttu Ehn <perttu.ehn@exove.fi>
2021-03-16 12:54:32 +0200

    Wrap command and expose it in verbose mode to the UI

--- docker/scripts/ld.command.solr-config-update.sh
+++ docker/scripts/ld.command.solr-config-update.sh
@@ -4,7 +4,12 @@
 # This file contains solr-config-update -command for local-docker script ld.sh.
 
 function ld_command_solr-config-update_exec() {
-    docker-compose exec ${CONTAINER_SOLR:-solr} bash -c "cp /solr-config/conf/* /var/solr/data/${SOLR_CORE}/conf"
+
+    COMM="docker-compose exec ${CONTAINER_SOLR:-solr} bash -c \"cp /solr-config/conf/* /var/solr/data/${SOLR_CORE}/conf\""
+    [ "$LD_VERBOSE" -ge "2" ] && echo -e "${Cyan}Next: $COMM${Color_Off}"
+    $COMM
+
+    [ "$LD_VERBOSE" -ge "2" ] && echo -e "${Cyan}Next: Restarting Solr container, please wait...${Color_Off}"
     docker-compose restart ${CONTAINER_SOLR:-solr}
 }
 
